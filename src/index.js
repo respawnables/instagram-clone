59e057ddbad17d66c96babdfc0d33a7fe9a14d59
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2021-05-31 02:17:36 +0300

    create routes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { firebase, FieldValue } from './lib/firebase';
 
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-    <App />,
+    <App />
   </FirebaseContext.Provider>,
   document.getElementById('root')
 );
---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
+export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  return <p>I am the login page</p>;
+}
